f1542a860110175f5608f8022abd656085a57663
piotrkrzyminski <piotr.krzyminski96@gmail.com>
2017-12-18 21:57:56 +0100

    Added Marcin Ciura gap sequence Added generating random values method Added array with random values

--- shell/src/main/java/pl/piotrkrzyminski/Main.java
+++ shell/src/main/java/pl/piotrkrzyminski/Main.java
@@ -2,6 +2,33 @@ package pl.piotrkrzyminski;
 
 public class Main {
     public static void main(String[] args) {
+        long[] sortArray = new long[100];
+        int[] gapsArray = {701, 301, 132, 57, 23, 10, 4, 1};
+        fillArray(sortArray);
 
+        System.out.println("Before sorting");
+        print(sortArray);
+
+        try {
+            Shellsort.sort(sortArray,gapsArray);
+        } catch (SortException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("After sorting");
+        print(sortArray);
+    }
+
+    public static void fillArray(long[] array) {
+        for(int i=0; i<array.length; i++) {
+            array[i] = (int)(Math.random() * 1000 + 1);
+        }
+    }
+
+    public static void print(long[] array) {
+        for(long value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.print('\n');
     }
 }
